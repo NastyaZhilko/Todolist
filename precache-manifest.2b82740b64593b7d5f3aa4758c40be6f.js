04a3eab21b6f3ef28f67ee16aa6e2be8aee87b3e
NastyaZhilko <nasten4ik2010@gmail.com>
2020-11-29 15:02:01 +0300

    Updates

---
+++ precache-manifest.2b82740b64593b7d5f3aa4758c40be6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072b4e5a93aeeb3bb14825bcbebbb7c9",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "4533696d6db54968f3bb",
+    "url": "/Todolist/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5dbbadec7acf5eff217a",
+    "url": "/Todolist/static/js/2.3b821b3d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Todolist/static/js/2.3b821b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4533696d6db54968f3bb",
+    "url": "/Todolist/static/js/main.570e7996.chunk.js"
+  },
+  {
+    "revision": "09577d2dc8c4bcb545ac",
+    "url": "/Todolist/static/js/runtime-main.7138dab9.js"
+  }
+]);
